afb2e64d6346eabd54966f7687fe8fa102132926
TsiaThe <th.tsiantas@icloud.com>
2021-09-20 12:03:24 +0200

    My first Java commit

--- StartingWindow.java
+++ StartingWindow.java
@@ -1,7 +1,9 @@
 import java.awt.Frame;
-public class TestFrame{
-         public static void main(String[] args) {
-                  Frame f = new Frame("Ex. 9.2");
+public class TestFrame
+{
+         public static void main(String[] args) 
+         {
+                  Frame f = new Frame("Ex. 9.3");
                   f.setSize(250,100);
                   f.setVisible(true);
          }
