8736dd8e8b47a47664a6f6dca879376bef2f4370
TsiaThe <th.tsiantas@icloud.com>
2021-09-20 12:18:26 +0200

    My second Java commit.

--- StartingWindow.java
+++ StartingWindow.java
@@ -3,7 +3,7 @@ public class TestFrame
 {
          public static void main(String[] args) 
          {
-                  Frame f = new Frame("Ex. 9.3");
+                  Frame f = new Frame("Ex. 10.3");
                   f.setSize(250,100);
                   f.setVisible(true);
          }
